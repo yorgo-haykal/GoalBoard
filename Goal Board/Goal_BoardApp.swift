b285015b95290e9e965421795115a5678eac8392
Yorgo Haykal <yorgo.haykal@universite-paris-saclay.fr>
2025-08-01 01:18:24 +0300

    Initial Commit

---
+++ Goal Board/Goal_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_BoardApp.swift
+//  Goal Board
+//
+//  Created by Yorgo Haykal on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Goal_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
